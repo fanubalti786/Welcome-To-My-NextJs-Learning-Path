5abf9386ab0d3331b3185f88c865a672d86bb2b7
fanubalti786 <fanubalti786@gmail.com>
2024-08-28 16:51:50 +0500

    inwhich we just learn what is root directories and how we can connect to each other like layout and page.tsx directires

--- class1/app/layout.tsx
+++ class1/app/layout.tsx
@@ -1,11 +1,16 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import React from 'react'
+
+export default function layout(props: any) {
+  console.log("props", props);
   return (
-    <html lang="en">
-      <body>{children}</body>
+    
+      <html lang="en">
+      <body>
+        <h1>{props.children}</h1>
+        <p>sahi kahty ho ap</p>
+      </body>
     </html>
-  );
+    
+  )
 }
+
