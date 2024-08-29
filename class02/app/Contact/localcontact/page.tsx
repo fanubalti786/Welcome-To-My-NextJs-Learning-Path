574f644423840b7f07e3c30d8881585291fac9a6
fanubalti786 <fanubalti786@gmail.com>
2024-08-29 16:53:31 +0500

    nested Routing to other routers navigation is possible through Link

---
+++ class02/app/Contact/localcontact/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+export default function local() {
+  return (
+    <div>
+      Local Contact page
+      <Link href="/Contact">Contact</Link>
+
+    </div>
+  )
+}
