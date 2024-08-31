fe1d2c5a73f4dff4ab56ed3822089bba80a9deee
fanubalti786 <fanubalti786@gmail.com>
2024-08-31 16:55:08 +0500

    when erroroccur any place all file will be destroy with after and before content

--- class03/app/Home/page.tsx
+++ class03/app/Home/page.tsx
@@ -3,7 +3,7 @@ import Button from '../Components/page'
 export default function Home() {
   return (
     <div>
-
+      <div>Hello</div>
       <div>Home Page {name}</div>
       <Button/>
     </div>
