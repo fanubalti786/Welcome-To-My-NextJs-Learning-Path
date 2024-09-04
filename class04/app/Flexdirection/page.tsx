9c898d68ad53f84a35ec6bdbee7b6245b14cb54e
fanubalti786 <fanubalti786@gmail.com>
2024-09-04 19:45:49 +0500

    Flexdirection

---
+++ class04/app/Flexdirection/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function News() {
+  return (
+    <div>
+      <div className="flex">
+  <div className="flex-none w-14 border-2 bg-orange-400 ">
+    01
+  </div>
+  <div className="flex-auto w-64 border-2 bg-orange-400 ">
+    02
+  </div>
+  <div className="flex-1 w-32 border-2 bg-orange-400 ">
+    03
+  </div>
+</div>
+    </div>
+  )
+}
--- class04/app/layout.tsx
+++ class04/app/layout.tsx
@@ -16,29 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
 
       <div className="bg-blue-100" >
         <div  className="flex flex-col h-[100vh]">
 
           <div className="bg-slate-500">
-          <ul className="flex justify-around text-white">
-            <li>Home</li>
-            <li>Blok</li>
-            <li>Post</li>
-            <li>Contact Us</li>
+          <ul className="flex justify-left gap-1 text-white">
+            <li className="p-2">Home</li>
+            <li className="p-2 px-4">Blog</li>
+            <li className="p-2 px-4">Post</li>
+            <li className="p-2">Contact Us</li>
           </ul>
           </div>
 
           <div className="flex-1 bg-purple-400">
-            <div className="flex h-[80vh]">
+            <div className="flex h-[80vh] justify-center">
 
-              <div className="bg-yellow-400 w-[250px] text-center">
+              <div className="bg-yellow-400 w-[250px]  border-2">
                 Side Navigation
               </div>
 
-              <div className="bg-slate-300 flex-1 text-center">
-                Main Content
+              <div className="bg-slate-300 flex-1  border-2">
+                {children}
               </div>
             </div>
            
