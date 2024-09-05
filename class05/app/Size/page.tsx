409fbabb21cc282cdde2339266c46bd12bd0daab
fanubalti786 <fanubalti786@gmail.com>
2024-09-05 16:05:01 +0500

    max and min width and hight

--- class05/app/Size/page.tsx
+++ class05/app/Size/page.tsx
@@ -40,6 +40,31 @@ export default function Size() {
         
       </div>
 
+
+
+      <div className=" flex border-2 bg-red-500 max-w-[500px] max-h-[500px] justify-center items-center ">
+
+        <div className="boder-2 bg-gray-700 w-1/2 h-1/2 text-center text-wrap">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad odit neque
+        voluptatum eaque dolores obcaecati alias delectus laboriosam. Labore
+        mollitia sint deserunt aliquid amet quas molestias veniam aspernatur
+        quidem laborum?
+        </div>
+        
+      </div>
+
+
+      <div className=" flex border-2 bg-red-500 min-w-[500px] min-h-[500px] justify-center items-center ">
+
+        <div className="boder-2 bg-gray-700 size-1/2 text-center text-wrap">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad odit neque
+        voluptatum eaque dolores obcaecati alias delectus laboriosam. Labore
+        mollitia sint deserunt aliquid amet quas molestias veniam aspernatur
+        quidem laborum?
+        </div>
+        
+      </div>
+
       
     </div> 
 
