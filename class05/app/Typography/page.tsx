b02494d129388760271e4265885cbe75a8a21d45
fanubalti786 <fanubalti786@gmail.com>
2024-09-05 12:46:22 +0500

    Typography properties like Font family,size,smoothing,style,weight,letterspacing

--- class05/app/Typography/page.tsx
+++ class05/app/Typography/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function Typographi() {
   return (
     <div>
-      
+      <h1 className="font-mono hover:font-sans text-2xl font-bold ">
+        Garlic bread with cheese: What the science tells us
+      </h1>
+
+      <p className="subpixel-antialiased">The quick brown fox ...</p>
+      <p className="antialiased hover:subpixel-antialiased">
+        The quick brown fox ...
+      </p>
+
+
+        <hr />
+        <br />
+    <h1 className="font-bold">Letter Spacing</h1>
+      <p className="tracking-tight ...">The quick brown fox ...</p>
+      <p className="tracking-normal ...">The quick brown fox ...</p>
+      <p className="tracking-wide ...">The quick brown fox ...</p>
     </div>
-  )
+  );
 }
