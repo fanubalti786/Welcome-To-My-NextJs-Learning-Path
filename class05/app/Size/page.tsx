3fba948d70a6ca7065c169a82354fdd99a703c93
fanubalti786 <fanubalti786@gmail.com>
2024-09-05 15:56:11 +0500

    width properties

---
+++ class05/app/Size/page.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import thumbnail from "@/app/Assets/Image/Nextjs.png";
+export default function Size() {
+  return (
+    <div>
+      <div className=" flex border-2 bg-red-500 w-[100vw] h-[100vh] justify-center items-center">
+
+        <div className="boder-2 bg-gray-700 w-[70vw] h-[70vh] text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad odit neque
+        voluptatum eaque dolores obcaecati alias delectus laboriosam. Labore
+        mollitia sint deserunt aliquid amet quas molestias veniam aspernatur
+        quidem laborum?
+        </div>
+        
+      </div>
+
+
+
+      <div className=" flex border-2 bg-red-500 w-[100vw] h-[100vh] justify-center items-center">
+
+        <div className="boder-2 bg-gray-700 w-1/2 h-1/2 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad odit neque
+        voluptatum eaque dolores obcaecati alias delectus laboriosam. Labore
+        mollitia sint deserunt aliquid amet quas molestias veniam aspernatur
+        quidem laborum?
+        </div>
+        
+      </div>
+
+
+
+      <div className=" flex border-2 bg-red-500 w-full h-full justify-center items-center">
+
+        <div className="boder-2 bg-gray-700 w-1/2 h-1/2 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad odit neque
+        voluptatum eaque dolores obcaecati alias delectus laboriosam. Labore
+        mollitia sint deserunt aliquid amet quas molestias veniam aspernatur
+        quidem laborum?
+        </div>
+        
+      </div>
+
+      
+    </div> 
+
+
+
+
+
+  );
+}
