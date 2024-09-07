2626cd87f19251526f0a2dd17083aed205e660d2
fanubalti786 <fanubalti786@gmail.com>
2024-09-07 16:21:21 +0500

    we cant change UI through simple variable

--- class06/app/component/page.tsx
+++ class06/app/component/page.tsx
@@ -1,14 +1,26 @@
 "use client"
 import React from 'react'
 
+// export default function Button() {
+
+//     const counter = 0
+//   return (
+//     <div>
+//        <button className='bg-gray-600 p-7' onClick={()=> console.log("hello")} >+</button>
+//       <span className="p-3">{counter}</span>
+//       <button className='bg-gray-600 p-7 px-8' onClick={()=> {console.log("True")}}>-</button>
+//     </div>
+//   )
+// }
+
 export default function Button() {
 
     const counter = 0
   return (
     <div>
-       <button className='bg-gray-600 p-7' onClick={()=> console.log("hello")} >+</button>
+       <button className='bg-gray-600 p-7' onClick={()=> {counter === counter + 1}} >+</button>
       <span className="p-3">{counter}</span>
-      <button className='bg-gray-600 p-7 px-8' onClick={()=> {console.log("True")}}>-</button>
+      <button className='bg-gray-600 p-7 px-8' onClick={()=> {counter === counter - 1}}>-</button>
     </div>
   )
 }
