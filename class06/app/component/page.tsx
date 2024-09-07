e4c8f422218d73c8907382200e27c41fce67ba38
fanubalti786 <fanubalti786@gmail.com>
2024-09-07 16:18:20 +0500

    how we can update ui through basic inline event listner

--- class06/app/component/page.tsx
+++ class06/app/component/page.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 
 export default function Button() {
+
+    const counter = 0
   return (
     <div>
-       <button className='bg-gray-600 p-7' >+</button>
-      <span className="p-3">counter</span>
-      <button className='bg-gray-600 p-7 px-8'>-</button>
+       <button className='bg-gray-600 p-7' onClick={()=> console.log("hello")} >+</button>
+      <span className="p-3">{counter}</span>
+      <button className='bg-gray-600 p-7 px-8' onClick={()=> {console.log("True")}}>-</button>
     </div>
   )
 }
