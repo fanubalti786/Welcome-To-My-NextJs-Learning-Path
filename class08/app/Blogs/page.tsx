89da0da84348d2ed76aad1ea0c5137ee67b08b64
fanubalti786 <fanubalti786@gmail.com>
2024-09-15 19:10:51 +0500

    Post Api triger through useffect

--- class08/app/Blogs/page.tsx
+++ class08/app/Blogs/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import PostCards from '../Components/TopNave/PostCards/PostCards'
 
 export default function Blogs() {
+
+    useEffect(()=>
+    {
+
+        getAllPost()
+
+    },[])
+
+    const getAllPost = async() =>{
+        const responce = await fetch("https://jsonplaceholder.typicode.com/posts")
+        console.log(responce)
+    }
   return (
     <div>
-      Blogs Page
+      <PostCards/>
     </div>
   )
 }
---
+++ class08/app/Components/TopNave/PostCards/PostCards.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function PostCards() {
+  return (
+    <div>
+      <div>
+            <div className="flex">
+                <div className="max-w-sm rounded shadow-lg bg-white m-4">
+                    {/* Post Image */}
+                    <img
+                        className="w-full"
+                        src="https://via.placeholder.com/400x200"
+                        alt="Post Image"
+                    />
+
+                    {/* Post Content */}
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Post Title</div>
+                        <p className="text-gray-700 text-base">
+                            Post Body
+                        </p>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
