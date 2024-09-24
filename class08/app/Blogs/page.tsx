1d67f9b45a5b92a50ffb661287479dee2258015a
fanubalti786 <fanubalti786@gmail.com>
2024-09-24 16:35:47 +0500

    update lecture 08

--- class08/app/Blogs/page.tsx
+++ class08/app/Blogs/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PostCards from '../Components/TopNave/PostCards/PostCards'
 import Link from 'next/link'
 
