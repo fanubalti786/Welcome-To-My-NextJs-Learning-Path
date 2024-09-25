ea8254da1078f1aa93d89291ed31cc6314c46bd2
fanubalti786 <fanubalti786@gmail.com>
2024-09-25 15:26:53 +0500

    how we can do prop drilling first comment

---
+++ class10/app/Components/ParentCom/ChildCom/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GrandChilCom from '../GrantChildCom/page'
+
+
+export default function ChildCom() {
+  return (
+    <div>
+      ChildCom Content:
+      <GrandChilCom/>
+    </div>
+  )
+}
---
+++ class10/app/Components/ParentCom/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChildCom from './ChildCom/page'
+
+
+export default function ParentCom() {
+  return (
+    <div>
+      ParentCom Context:
+      <ChildCom/>
+    </div>
+  )
+}
