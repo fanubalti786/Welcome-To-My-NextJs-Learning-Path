b15c64191e720c716f43767ed852a4eb5d842ff1
fanubalti786 <fanubalti786@gmail.com>
2024-09-27 22:39:41 +0500

    update

--- class10/Context/MyContext.tsx
+++ class10/Context/MyContext.tsx
@@ -1,9 +1,10 @@
 "use client"
 import { createContext, useState } from "react";
 
-export const Mycontext = createContext();
+export const Mycontext = createContext({});
 
 export function ContextProvider({children}:any){
+    
     const [name,nameState] = useState("Fanu Balti")
 
     return(
