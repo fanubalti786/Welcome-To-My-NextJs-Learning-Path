e2812d4edbbf9da8f5cdf5a507bfcbc6722d6577
fanubalti786 <fanubalti786@gmail.com>
2024-09-27 22:32:41 +0500

    update

--- class10/app/Components/ParentCom/GrantChildCom/page.tsx
+++ class10/app/Components/ParentCom/GrantChildCom/page.tsx
@@ -4,7 +4,7 @@ import React,{useContext} from 'react'
 
 export default function GrandChilCom() {
   const ourContext:any = useContext(Mycontext)
-  const {name, setName} = ourContext
+  const {name, setdata} = ourContext
   console.log(ourContext)
   return (
     <div>
