7ba2e733a864aeb86961066f043ed2e1ead7ae02
fanubalti786 <fanubalti786@gmail.com>
2024-10-05 13:10:13 +0500

    onchange function done on textfiled

--- class11/app/page.tsx
+++ class11/app/page.tsx
@@ -1,6 +1,15 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+const [toDoItems,setToDoItems] = useState([])
+const [inputTextField,setInputTextField] = useState("")
+console.log(inputTextField)
+
+
+
   return (
     <div className="flex justify-center items-center h-screen bg-teal-700 font-bold">
       <div className=" flex flex-col border-1 border-white rounded-[5px] w-[450px] h-[250px] bg-slate-600">
@@ -8,11 +17,14 @@ export default function Home() {
 
         <div className=" flex-1 border-2 border-slate-700 ">
           <div className="flex mt-7 flex-col justify-center items-center">
+
+
             <div className="">
               <input 
                 className="rounded-[3px] px-9 py-1.5 bg-slate-200"
                 type="text"
                 placeholder="Per Day Task"
+                onChange={(e)=> setInputTextField(e.target.value)}
 
               />
               <button className=" ml-0.5 rounded-[3px] px-3 py-1.5 text-white bg-red-900 ">Done</button>
@@ -24,20 +36,10 @@ export default function Home() {
                 <button className="border-3 border-green-700 px-1 py-0.5 mt-[3px] mr-[2px]  rounded-[3px] bg-green-700">Complete</button>
                 <button className="border-2 border-red-800 px-1   mt-[3px] rounded-[3px] bg-red-700">Remove</button>
                 </div>
-            
-                
               </ul>
-
-
               </div>
-              
 
-              
             </div>
-
-
-           
-
 
           </div>
 
