922932de67ca2e4fb41f6c8cfbe6536e65f2e03c
fanubalti786 <fanubalti786@gmail.com>
2024-10-14 18:39:14 +0500

    All Api calling don throgh server side rendering

--- class12/src/app/Blogs/[blogId]/page.tsx
+++ class12/src/app/Blogs/[blogId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import Comments from '@/app/Components/TopNave/Comments/page';
 import PostCards from '@/app/Components/TopNave/PostCards/PostCards'
 import React, { useEffect, useState } from 'react'
@@ -10,46 +10,41 @@ import React, { useEffect, useState } from 'react'
 //   body: string;
 // }
 
-export default function blogIds({params}:any) {
 
-  const [postData, setPostData] = useState<any>({})
-  const [comments, setComments] = useState([])
-
-  useEffect(()=> {
-
-    getspecificpost()
-
-  },[])
-
-  const getspecificpost = async () => {
-    const responce = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.blogId}`)
-    if(responce.ok){
-      const specificData = await responce.json()
-      setPostData(specificData)
-      console.log(specificData)
-      const responseComment = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.blogId}/comments`)
-      const commentsData = await responseComment.json()
-      console.log("comments", commentsData)
-      setComments(commentsData)
+const getspecificpost = async (params:any) => {
+  const responce = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.blogId}`)
+  if(responce.ok){
+    const specificData = await responce.json()
+    const responseComment = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.blogId}/comments`)
+    const commentsData = await responseComment.json()
+    return{
+      specificData:specificData,
+      commentsData:commentsData
     }
-    
-    
-
   }
+  
+  
+
+}
+export default async function blogIds({params}:any) {
+
+ const data = await getspecificpost(params)
+
+  
   
   return (
     <div>
             <div className="w-full mx-auto bg-white shadow-md rounded-lg my-4 p-6">
                 {/* Post Title */}
                 <h2 className="font-bold ">Tittle</h2>
-                <h6 className="text-2xl font-bold text-gray-800 mb-4">{postData.title}</h6>
+                <h6 className="text-2xl font-bold text-gray-800 mb-4">{data?.specificData.title}</h6>
 
                 {/* Post Body */}
                 <h1 className='font-bold'>Body</h1>
-                <p className="text-gray-700 text-base mb-6">{postData.body}</p>
+                <p className="text-gray-700 text-base mb-6">{data?.commentsData.body}</p>
 
                 {/* Comments Section */}
-                {comments.map((com) => {
+                {data?.commentsData.map((com:any) => {
                     return (
                         <Comments commentData={com} />
                     )
