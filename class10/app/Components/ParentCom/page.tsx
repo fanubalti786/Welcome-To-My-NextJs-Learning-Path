7bd574610134d6818c52a9eb2627f37c21f45052
fanubalti786 <fanubalti786@gmail.com>
2024-10-14 14:18:56 +0500

    update

--- class10/app/Components/ParentCom/page.tsx
+++ class10/app/Components/ParentCom/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+import { Mycontext } from '@/Context/MyContext'
+import { useContext } from 'react'
 import React from 'react'
 import ChildCom from './ChildCom/page'
 
 
 export default function ParentCom() {
+  const ourContext:any = useContext(Mycontext)
+  const {name,setName} = ourContext
   return (
     <div>
-      ParentCom Context:
+      ParentCom Context: {name}
       <ChildCom/>
     </div>
   )
