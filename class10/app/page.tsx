343cfaa2570d7b792291370d100f7c569d62f9d5
fanubalti786 <fanubalti786@gmail.com>
2024-10-19 14:53:32 +0500

    update

--- class10/app/page.tsx
+++ class10/app/page.tsx
@@ -1,28 +1,21 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { ContextProvider } from "@/Context/MyContext";
 import ParentCom from "./Components/ParentCom/page";
 import { Mycontext } from "@/Context/MyContext";
 import { useContext } from "react";
 
-
-
-
 export default function Home() {
-const {name,setName}:any = useContext(Mycontext)
+  const { name, setName }: any = useContext(Mycontext);
 
   return (
-   <div>
-    {/* Not be working i don't know */}
-    {/* <div>Home:{name}</div> */}
     <ContextProvider>
-
-        <ParentCom/>
-
+      <div>
+        <div>
+          <h1>Home:{name}</h1>
+        </div>
+        <ParentCom />
+      </div>
     </ContextProvider>
-     
-   </div>
-   
-
   );
 }
