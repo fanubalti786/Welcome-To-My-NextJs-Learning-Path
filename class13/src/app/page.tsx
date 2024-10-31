c14af09886f33cfc4ad36ce697d589ca3b09542c
fanubalti786 <fanubalti786@gmail.com>
2024-10-31 21:34:12 +0500

    update

--- class13/src/app/page.tsx
+++ class13/src/app/page.tsx
@@ -5,12 +5,12 @@ import { useSelector } from 'react-redux'
 
 
 export default function page() {
-  const counterState = useSelector((state:any) => state)
+  const counterState = useSelector((state:any) => state.counter.CounterSlicerReducer)
   console.log(counterState)
   return (
     
     
-      <div className='border-2 '>hello</div>
+      <div className='border-2 '>hello{counterState}</div>
 
     
       
