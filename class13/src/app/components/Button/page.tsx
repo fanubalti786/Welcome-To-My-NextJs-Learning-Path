c59cc282941350a845f01baeda382f97bdab364f
fanubalti786 <fanubalti786@gmail.com>
2024-11-02 19:15:57 +0500

    update

---
+++ class13/src/app/components/Button/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+
+export default function Button() {
+  const counterState = useSelector((state:any) => state.Counter)
+  console.log(counterState)
+  return (
+    
+    
+      <div className='border-2 '>
+        
+        Counter-{counterState.counter} and  Adder-{counterState.adder}
+        <div>
+            <button>MainComponentButton</button>
+        </div>
+
+      
+      
+      </div>
+      
+
+    
+  )
+}
