6571ab9b59c5c0b5d0d772b15eecbd4a77854771
fanubalti786 <fanubalti786@gmail.com>
2024-11-02 19:14:59 +0500

    update

---
+++ class13/src/app/About/components/Form/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { incrementCounter} from '@/store/counter/counterSlicer'
+import Button2 from '../Button/page'
+
+
+
+export default function Form() {
+  const counterState = useSelector((state:any) => state.Counter)
+  const dispatch = useDispatch()
+  const onClickHandler = ()=>
+  {
+    dispatch(incrementCounter())
+    
+
+  }
+  return (
+    
+    
+      <div className='border-2 '>
+        Counter-{counterState.counter} 
+
+        <div><button onClick={onClickHandler} className={'border-2 border-black p-1 px-2 m-1 bg-green-500'}  >Click</button></div>
+        
+        <hr />
+        <div>
+            <button>AboutComponentForm</button>
+            <hr />
+            
+
+            <hr />
+            
+        </div>
+        
+        </div>
+
+    
+  )
+}
---
+++ class13/src/app/About/components/User/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+
+export default function User() {
+  const counterState = useSelector((state:any) => state.Counter)
+  console.log(counterState)
+  return (
+    
+    
+      <div className='border-2 '>Counter-{counterState.counter} and  Adder-{counterState.adder}
+        
+        <hr />
+        <div>
+            <button>AboutComponentButton</button>
+        </div>
+        
+        </div>
+
+    
+  )
+}
