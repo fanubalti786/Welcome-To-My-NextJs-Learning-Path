a765911a971143c8588ff6eb70ad74ba88865bb4
fanubalti786 <fanubalti786@gmail.com>
2024-11-02 19:16:58 +0500

    update

--- class13/src/app/page.tsx
+++ class13/src/app/page.tsx
@@ -1,16 +1,46 @@
 "use client"
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { Provider} from 'react-redux'
+import Form from './About/components/Form/page'
+import store from '@/store/store'
+import Button from './components/Button/page'
+import User from './About/components/User/page'
+import { incrementCounter } from '@/store/counter/counterSlicer'
+import { useDispatch} from 'react-redux'
+
 
 
 
 export default function page() {
-  const counterState = useSelector((state:any) => state.Counter)
-  console.log(counterState)
+
+  // const dispatch = useDispatch()
+
+  // const onClickHandler = () =>
+  // {
+  //   dispatch(incrementCounter())
+  // }
+  
   return (
     
     
-      <div className='border-2 '>Counter-{counterState.counter} and  Adder-{counterState.adder}</div>
+      <div className='border-2 '>Counter-{} and  Adder-{}
+      <hr />
+
+      <div>
+        <Provider store={store}>
+          {<Form/>}
+        </Provider>
+
+        <button className='border-2 px-3 py-2 '>clickMe!</button>
+
+        {/* <Button/> */}
+
+        {/* <User/> */}
+
+
+      </div>
+
+      </div>
 
     
       
